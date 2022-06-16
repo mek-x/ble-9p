661d00edc961ba04d016e4cafb18cc5737fa4ea7
Kamil Wcisło <mek.xgt@gmail.com>
2022-06-17 01:03:54 +0200

    initial skeleton

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/mek-x/ble-9p
+
+go 1.18
+
+require github.com/knusbaum/go9p v1.18.0
+
+require (
+	9fans.net/go v0.0.2 // indirect
+	github.com/Plan9-Archive/libauth v0.0.0-20180917063427-d1ca9e94969d // indirect
+	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	github.com/fhs/mux9p v0.3.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/knusbaum/go9p"
+	"github.com/knusbaum/go9p/fs"
+)
+
+type randomFile struct {
+	fs.BaseFile
+}
+
+func (f *randomFile) Read(fid uint64, offset uint64, count uint64) ([]byte, error) {
+	bs := make([]byte, count)
+	rand.Read(bs)
+	return bs, nil
+}
+
+func main() {
+
+	go9p.Verbose = true
+
+	extendedFS, root := fs.NewFS("glenda", "glenda", 0777,
+		fs.WithCreateFile(fs.CreateStaticFile),
+		fs.WithCreateDir(fs.CreateStaticDir),
+		fs.WithRemoveFile(fs.RMFile),
+		fs.IgnorePermissions(),
+		//fs.WithAuth(fs.Plan9Auth),
+		// fs.WithAuth(fs.PlainAuth(map[string]string{
+		// 	"kyle": "foo",
+		// 	"jake": "bar",
+		// })),
+	)
+
+	root.AddChild(fs.NewStaticFile(
+		extendedFS.NewStat("static", "glenda", "glenda", 0666),
+		[]byte("Hello, World!\n"),
+	))
+
+	root.AddChild(fs.NewDynamicFile(
+		extendedFS.NewStat("dynamic", "glenda", "glenda", 0666),
+		func() []byte {
+			return []byte(time.Now().String() + "\n")
+		},
+	))
+
+	root.AddChild(&randomFile{
+		*fs.NewBaseFile(extendedFS.NewStat("baseRand", "glenda", "glenda", 0666)),
+	})
+
+	root.AddChild(&fs.WrappedFile{
+		File: fs.NewBaseFile(extendedFS.NewStat("wrappedRand", "glenda", "glenda", 0666)),
+		ReadF: func(fid uint64, offset uint64, count uint64) ([]byte, error) {
+			bs := make([]byte, count)
+			rand.Read(bs)
+			return bs, nil
+		},
+	})
+
+	log.Println(go9p.Serve("0.0.0.0:5640", extendedFS.Server()))
+}
